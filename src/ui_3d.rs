b8bfe9e99d24424f293b836fb80aae02bf97bd81
bgfxc4 <bindermarc@gmx.de>
2023-03-04 04:03:11 +0100

    added basic shaders

--- src/ui_3d.rs
+++ src/ui_3d.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use eframe::egui_glow;
 use egui::mutex::Mutex;
 use egui_glow::glow;
+use glow::*;
 
 pub struct UI3d {
     /// Behind an `Arc<Mutex<…>>` so we can pass it to [`egui::PaintCallback`] and paint later.
@@ -40,7 +41,7 @@ impl UI3d {
             ui.allocate_exact_size(ui.available_size(), egui::Sense::hover());
 
         self.angle = match response.hover_pos() {
-            Some(pos) => self.angle + pos.x * 0.01,
+            Some(pos) => pos.x,
             None => 0.0
         };
 
@@ -61,120 +62,171 @@ impl UI3d {
 }
 
 struct RotatingTriangle {
-    program: glow::Program,
-    vertex_array: glow::VertexArray,
+    program: Program,
+    vertex_array: VertexArray,
+    vertex_buffer_object: NativeBuffer
 }
 
 #[allow(unsafe_code)] // we need unsafe code to use glow
 impl RotatingTriangle {
-    fn new(gl: &glow::Context) -> Option<Self> {
-        use glow::HasContext as _;
+    fn new(gl: &Context) -> Option<Self> {
+        use HasContext as _;
 
         let shader_version = egui_glow::ShaderVersion::get(gl);
-
+        println!("{}", shader_version.version_declaration());
         unsafe {
-            let program = gl.create_program().expect("Cannot create program");
-
             let (vertex_shader_source, fragment_shader_source) = (
-                r#"
-                    const vec2 verts[3] = vec2[3](
-                        vec2(0.0, 1.0),
-                        vec2(-1.0, -1.0),
-                        vec2(1.0, -1.0)
-                    );
-                    const vec4 colors[3] = vec4[3](
-                        vec4(1.0, 0.0, 0.0, 1.0),
-                        vec4(0.0, 1.0, 0.0, 1.0),
-                        vec4(0.0, 0.0, 1.0, 1.0)
-                    );
-                    out vec4 v_color;
-                    uniform float u_angle;
+                format!("{}\n{}", shader_version.version_declaration(), r#"
+                    in vec3 in_position;
+                    in vec2 in_x_y_rot;
+
+                    out vec2 position;
+
                     void main() {
-                        v_color = colors[gl_VertexID];
-                        gl_Position = vec4(verts[gl_VertexID], 0.0, 1.0);
-                        gl_Position.x *= cos(u_angle);
+                        position = vec2(in_position.x, in_position.y);
+                        gl_Position = vec4(in_position.x - 0.5, in_position.y - 0.5, 0.0, 1.0);
                     }
-                "#,
-                r#"
+                "#),
+                format!("{}\n{}", shader_version.version_declaration(), r#"
                     precision mediump float;
-                    in vec4 v_color;
-                    out vec4 out_color;
+                    in vec2 position;
+                    out vec4 color;
+                    uniform float blue;
                     void main() {
-                        out_color = v_color;
+                        color = vec4(position, blue, 1.0);
                     }
-                "#,
+                "#),
             );
 
-            let shader_sources = [
-                (glow::VERTEX_SHADER, vertex_shader_source),
-                (glow::FRAGMENT_SHADER, fragment_shader_source),
-            ];
+            let program = create_program(&gl, vertex_shader_source.as_str(), fragment_shader_source.as_str());
+            gl.use_program(Some(program));
 
-            let shaders: Vec<_> = shader_sources
-                .iter()
-                .map(|(shader_type, shader_source)| {
-                    let shader = gl
-                        .create_shader(*shader_type)
-                        .expect("Cannot create shader");
-                    gl.shader_source(
-                        shader,
-                        &format!(
-                            "{}\n{}",
-                            shader_version.version_declaration(),
-                            shader_source
-                        ),
-                    );
-                    gl.compile_shader(shader);
-                    assert!(
-                        gl.get_shader_compile_status(shader),
-                        "Failed to compile custom_3d_glow {shader_type}: {}",
-                        gl.get_shader_info_log(shader)
-                    );
+            // Create a vertex buffer and vertex array object
+            let (vbo, vao) = init_vertex_buffer(&gl);
+            
+            gl.clear_color(0.1, 0.2, 0.3, 1.0);
 
-                    gl.attach_shader(program, shader);
-                    shader
-                })
-                .collect();
-
-            gl.link_program(program);
-            if !gl.get_program_link_status(program) {
-                panic!("{}", gl.get_program_info_log(program));
-            }
-
-            for shader in shaders {
-                gl.detach_shader(program, shader);
-                gl.delete_shader(shader);
-            }
-
-            let vertex_array = gl
-                .create_vertex_array()
-                .expect("Cannot create vertex array");
+            // Upload some uniforms
+            set_uniform(&gl, program, "blue", 0.8);
 
             Some(Self {
                 program,
-                vertex_array,
+                vertex_array: vao,
+                vertex_buffer_object: vbo,
             })
         }
     }
 
-    fn destroy(&self, gl: &glow::Context) {
-        use glow::HasContext as _;
+    fn destroy(&self, gl: &Context) {
+        use HasContext as _;
         unsafe {
             gl.delete_program(self.program);
             gl.delete_vertex_array(self.vertex_array);
+            gl.delete_buffer(self.vertex_buffer_object);
         }
     }
 
-    fn paint(&self, gl: &glow::Context, angle: f32) {
-        use glow::HasContext as _;
+    fn paint(&self, gl: &Context, angle: f32) {
+        use HasContext as _;
         unsafe {
+            // gl.clear(COLOR_BUFFER_BIT);
+
             gl.use_program(Some(self.program));
-            gl.uniform_1_f32(
-                gl.get_uniform_location(self.program, "u_angle").as_ref(),
-                angle,
-            );
-            gl.bind_vertex_array(Some(self.vertex_array));
-            gl.draw_arrays(glow::TRIANGLES, 0, 3);
+
+            gl.clear(glow::COLOR_BUFFER_BIT);
+
+            self.fill_vertex_buffer(gl, angle);
+
+            gl.draw_arrays(LINE_LOOP, 0, 4);
         }
     }
+
+    unsafe fn fill_vertex_buffer(&self, gl: &Context, angle: f32) {
+        // let triangle_vertices = [0.5f32, 1.0f32, 0.0f32, 0.0f32, 1.0f32, (angle/100.0) % 3.0];
+        // let triangle_vertices_u8: &[u8] = core::slice::from_raw_parts(
+        //     triangle_vertices.as_ptr() as *const u8,
+        //     triangle_vertices.len() * core::mem::size_of::<f32>(),
+        // );
+        let stone_vertices = [
+            0.0f32, 0.0f32, 5.0f32,  5.0f32, 5.0f32,
+            1.0f32, 0.0f32, 5.0f32,  5.0f32, 5.0f32,
+            1.0f32, 0.5f32, 0.0f32,  0.0f32, 0.0f32,
+            0.5f32, 1.0f32, 0.0f32,  0.0f32, 0.0f32,
+        ];
+        let stone_vertices_u8: &[u8] = core::slice::from_raw_parts(
+            stone_vertices.as_ptr() as *const u8,
+            stone_vertices.len() * core::mem::size_of::<f32>(),
+        );
+
+        // upload the data
+        gl.bind_buffer(ARRAY_BUFFER, Some(self.vertex_buffer_object));
+        gl.buffer_data_u8_slice(ARRAY_BUFFER, stone_vertices_u8, STATIC_DRAW);
+
+        // We now construct a vertex array to describe the format of the input buffer
+        gl.bind_vertex_array(Some(self.vertex_array));
+        
+        gl.enable_vertex_attrib_array(0); //vec3 stone origin pos
+        gl.vertex_attrib_pointer_f32(0, 3, FLOAT, false, 20, 0);
+
+        gl.enable_vertex_attrib_array(1); //vec2 stone x and y rot
+        gl.vertex_attrib_pointer_f32(1, 2, FLOAT, false, 20, 12);
+    }
+}
+
+unsafe fn create_program(
+    gl: &Context,
+    vertex_shader_source: &str,
+    fragment_shader_source: &str,
+) -> NativeProgram {
+    let program = gl.create_program().expect("Cannot create program");
+
+    let shader_sources = [
+        (VERTEX_SHADER, vertex_shader_source),
+        (FRAGMENT_SHADER, fragment_shader_source),
+    ];
+
+    let mut shaders = Vec::with_capacity(shader_sources.len());
+
+    for (shader_type, shader_source) in shader_sources.iter() {
+        let shader = gl
+            .create_shader(*shader_type)
+            .expect("Cannot create shader");
+        gl.shader_source(shader, shader_source);
+        gl.compile_shader(shader);
+        if !gl.get_shader_compile_status(shader) {
+            panic!("{}", gl.get_shader_info_log(shader));
+        }
+        gl.attach_shader(program, shader);
+        shaders.push(shader);
+    }
+
+    gl.link_program(program);
+    if !gl.get_program_link_status(program) {
+        panic!("{}", gl.get_program_info_log(program));
+    }
+
+    for shader in shaders {
+        gl.detach_shader(program, shader);
+        gl.delete_shader(shader);
+    }
+
+    program
+}
+
+unsafe fn init_vertex_buffer(gl: &Context) -> (NativeBuffer, NativeVertexArray) {
+    // We now construct a vertex array to describe the format of the input buffer
+    let vao = gl.create_vertex_array().unwrap();
+    gl.bind_vertex_array(Some(vao));
+
+    // We construct a buffer
+    let vbo = gl.create_buffer().unwrap();
+    gl.bind_buffer(ARRAY_BUFFER, Some(vbo));
+
+    (vbo, vao)
+}
+
+unsafe fn set_uniform(gl: &Context, program: NativeProgram, name: &str, value: f32) {
+    let uniform_location = gl.get_uniform_location(program, name);
+    // See also `uniform_n_i32`, `uniform_n_u32`, `uniform_matrix_4_f32_slice` etc.
+    gl.uniform_1_f32(uniform_location.as_ref(), value)
 }
