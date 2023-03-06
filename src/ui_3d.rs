9a2bfcf078fbd5678ad8bf73f1a67274221ca0a3
bgfxc4 <bindermarc@gmx.de>
2023-03-06 13:36:04 +0100

    figured out projection and view mats

--- src/ui_3d.rs
+++ src/ui_3d.rs
@@ -7,22 +7,44 @@ use glow::*;
 
 pub struct UI3d {
     /// Behind an `Arc<Mutex<…>>` so we can pass it to [`egui::PaintCallback`] and paint later.
-    rotating_triangle: Arc<Mutex<RotatingTriangle>>,
+    canvas: Arc<Mutex<Canvas>>,
     angle: f32,
+    cam_pos: cgmath::Point3<f32>,
+    cam_look_at: cgmath::Point3<f32>,
 }
 
 impl UI3d {
     pub fn new<'a>(cc: &'a eframe::CreationContext<'a>) -> Option<Self> {
         let gl = cc.gl.as_ref()?;
         Some(Self {
-            rotating_triangle: Arc::new(Mutex::new(RotatingTriangle::new(gl)?)),
+            canvas: Arc::new(Mutex::new(Canvas::new(gl)?)),
             angle: 0.0,
+            cam_pos: cgmath::Point3{x: 1.0f32, y: 2.0f32, z: 2.0f32},
+            cam_look_at: cgmath::Point3{x: 0f32, y: 0f32, z: 0f32},
         })
     }
 }
 
 impl eframe::App for UI3d {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let (keys_down, screen_rect) = ctx.input(|i| (i.keys_down.to_owned(), i.screen_rect));
+        if keys_down.contains(&egui::Key::S) {
+            self.cam_pos.z += 0.01f32;
+            self.cam_look_at.z += 0.01f32;
+        }
+        if keys_down.contains(&egui::Key::W) {
+            self.cam_pos.z -= 0.01f32;
+            self.cam_look_at.z -= 0.01f32;
+        }
+        if keys_down.contains(&egui::Key::D) {
+            self.cam_pos.x += 0.01f32;
+            self.cam_look_at.x += 0.01f32;
+        }
+        if keys_down.contains(&egui::Key::A) {
+            self.cam_pos.x -= 0.01f32;
+            self.cam_look_at.x -= 0.01f32;
+        }
+
         egui::CentralPanel::default().show(ctx, |ui| {
             self.custom_painting(ui);
         });
@@ -30,7 +52,7 @@ impl eframe::App for UI3d {
 
     fn on_exit(&mut self, gl: Option<&glow::Context>) {
         if let Some(gl) = gl {
-            self.rotating_triangle.lock().destroy(gl);
+            self.canvas.lock().destroy(gl);
         }
     }
 }
@@ -47,10 +69,12 @@ impl UI3d {
 
         // Clone locals so we can move them into the paint callback:
         let angle = self.angle;
-        let rotating_triangle = self.rotating_triangle.clone();
+        let canvas = self.canvas.clone();
+        let cam_pos = self.cam_pos.clone();
+        let cam_look_at = self.cam_look_at.clone();
 
         let cb = egui_glow::CallbackFn::new(move |_info, painter| {
-            rotating_triangle.lock().paint(painter.gl(), angle);
+            canvas.lock().paint(painter.gl(), angle, cam_pos, cam_look_at);
         });
 
         let callback = egui::PaintCallback {
@@ -61,7 +85,7 @@ impl UI3d {
     }
 }
 
-struct RotatingTriangle {
+struct Canvas {
     program: Program,
     vertex_array: NativeVertexArray,
     vertex_buffer_object: NativeBuffer,
@@ -69,25 +93,25 @@ struct RotatingTriangle {
 }
 
 #[allow(unsafe_code)] // we need unsafe code to use glow
-impl RotatingTriangle {
+impl Canvas {
     fn new(gl: &Context) -> Option<Self> {
         use HasContext as _;
 
-        let shader_version = egui_glow::ShaderVersion::get(gl);
-        println!("{}", shader_version.version_declaration());
         unsafe {
             let (vertex_shader_source, fragment_shader_source) = (
                 r#"#version 330 core
-                layout (location = 0) in vec2 aPos;
+                layout (location = 0) in vec3 pos_model_space;
                 layout (location = 1) in vec3 aColor;
                 layout (location = 2) in vec2 aOffset;
+
+                uniform mat4 view_mat;
 
                 out vec3 fColor;
 
                 void main()
                 {
                     fColor = aColor;
-                    gl_Position = vec4(aPos + aOffset, 0.0, 1.0);
+                    gl_Position = view_mat * vec4(pos_model_space, 1.0);
                 }
                 "#,
                 r#"#version 330 core
@@ -109,9 +133,6 @@ impl RotatingTriangle {
             
             gl.clear_color(0.1, 0.2, 0.3, 1.0);
 
-            // Upload some uniforms
-            set_uniform(&gl, program, "blue", 0.8);
-
             Some(Self {
                 program,
                 vertex_array: vao,
@@ -130,19 +151,19 @@ impl RotatingTriangle {
         }
     }
 
-    fn paint(&self, gl: &Context, angle: f32) {
+    fn paint(&self, gl: &Context, angle: f32, cam_pos: cgmath::Point3<f32>, cam_look_at: cgmath::Point3<f32>) {
         use HasContext as _;
         unsafe {
             gl.clear(glow::COLOR_BUFFER_BIT);
 
-            let count = self.fill_vertex_buffer(gl, angle);
+            let count = self.fill_vertex_buffer(gl, angle, cam_pos, cam_look_at);
 
-            gl.draw_arrays_instanced(TRIANGLES, 0, 6, count.try_into().unwrap());
+            gl.draw_arrays_instanced(TRIANGLES, 0, 12*3, count.try_into().unwrap());
             gl.bind_vertex_array(None);
         }
     }
 
-    unsafe fn fill_vertex_buffer(&self, gl: &Context, angle: f32) -> usize {
+    unsafe fn fill_vertex_buffer(&self, gl: &Context, angle: f32, cam_pos: cgmath::Point3<f32>, cam_look_at: cgmath::Point3<f32>) -> usize {
         gl.use_program(Some(self.program));
         gl.bind_vertex_array(Some(self.vertex_array));
 
@@ -169,6 +190,14 @@ impl RotatingTriangle {
 
         gl.bind_buffer(ARRAY_BUFFER, None);
         gl.vertex_attrib_divisor(2, 1); // tell OpenGL this is an instanced vertex attribute
+                                        //
+        let uniform_location = gl.get_uniform_location(self.program, "view_mat");
+        let proj_mat = cgmath::perspective(cgmath::Deg(60f32), 16f32 / 9f32, 1f32, 10f32);
+        let view_mat = cgmath::Matrix4::look_at_rh(cam_pos, cam_look_at, cgmath::Vector3{x: 0f32, y: 1f32, z: 0f32});
+        let mat = proj_mat * view_mat;
+        let f32_mat: [[f32; 4]; 4] = mat.into();
+
+        gl.uniform_matrix_4_f32_slice(uniform_location.as_ref(), false, f32_mat.into_iter().flatten().collect::<Vec<f32>>().as_ref());
 
         100
     }
@@ -221,14 +250,42 @@ unsafe fn init_vertex_buffer(gl: &Context) -> (NativeBuffer, NativeBuffer, Nativ
     gl.bind_vertex_array(Some(vao));
 
     let quad_vertices = [
-    // positions     // colors
-    -0.05f32,  0.05f32,  1.0f32, 0.0f32, 0.0f32,
-     0.05f32, -0.05f32,  0.0f32, 1.0f32, 0.0f32,
-    -0.05f32, -0.05f32,  0.0f32, 0.0f32, 1.0f32,
-
-    -0.05f32,  0.05f32,  1.0f32, 0.0f32, 0.0f32,
-     0.05f32, -0.05f32,  0.0f32, 1.0f32, 0.0f32,   
-     0.05f32,  0.05f32,  0.0f32, 1.0f32, 1.0f32
+        -1.0f32,-1.0f32,-1.0f32,   1.0f32, 0.0f32, 0.0f32,
+        -1.0f32,-1.0f32, 1.0f32,   0.0f32, 1.0f32, 0.0f32,
+        -1.0f32, 1.0f32, 1.0f32,   0.0f32, 0.0f32, 1.0f32,
+        1.0f32, 1.0f32,-1.0f32,    1.0f32, 0.0f32, 0.0f32,
+        -1.0f32,-1.0f32,-1.0f32,   0.0f32, 1.0f32, 0.0f32,
+        -1.0f32, 1.0f32,-1.0f32,   0.0f32, 0.0f32, 1.0f32,
+        1.0f32,-1.0f32, 1.0f32,    1.0f32, 0.0f32, 0.0f32,
+        -1.0f32,-1.0f32,-1.0f32,   0.0f32, 1.0f32, 0.0f32,
+        1.0f32,-1.0f32,-1.0f32,    0.0f32, 0.0f32, 1.0f32,
+        1.0f32, 1.0f32,-1.0f32,    1.0f32, 0.0f32, 0.0f32,
+        1.0f32,-1.0f32,-1.0f32,    0.0f32, 1.0f32, 0.0f32,
+        -1.0f32,-1.0f32,-1.0f32,   0.0f32, 0.0f32, 1.0f32,
+        -1.0f32,-1.0f32,-1.0f32,   1.0f32, 0.0f32, 0.0f32,
+        -1.0f32, 1.0f32, 1.0f32,   0.0f32, 1.0f32, 0.0f32,
+        -1.0f32, 1.0f32,-1.0f32,   0.0f32, 0.0f32, 1.0f32,
+        1.0f32,-1.0f32, 1.0f32,    1.0f32, 0.0f32, 0.0f32,
+        -1.0f32,-1.0f32, 1.0f32,   0.0f32, 1.0f32, 0.0f32,
+        -1.0f32,-1.0f32,-1.0f32,   0.0f32, 0.0f32, 1.0f32,
+        -1.0f32, 1.0f32, 1.0f32,   1.0f32, 0.0f32, 0.0f32,
+        -1.0f32,-1.0f32, 1.0f32,   0.0f32, 1.0f32, 0.0f32,
+        1.0f32,-1.0f32, 1.0f32,    0.0f32, 0.0f32, 1.0f32,
+        1.0f32, 1.0f32, 1.0f32,    1.0f32, 0.0f32, 0.0f32,
+        1.0f32,-1.0f32,-1.0f32,    0.0f32, 1.0f32, 0.0f32,
+        1.0f32, 1.0f32,-1.0f32,    0.0f32, 0.0f32, 1.0f32,
+        1.0f32,-1.0f32,-1.0f32,    1.0f32, 0.0f32, 0.0f32,
+        1.0f32, 1.0f32, 1.0f32,    0.0f32, 1.0f32, 0.0f32,
+        1.0f32,-1.0f32, 1.0f32,    0.0f32, 0.0f32, 1.0f32,
+        1.0f32, 1.0f32, 1.0f32,    1.0f32, 0.0f32, 0.0f32,
+        1.0f32, 1.0f32,-1.0f32,    0.0f32, 1.0f32, 0.0f32,
+        -1.0f32, 1.0f32,-1.0f32,   0.0f32, 0.0f32, 1.0f32,
+        1.0f32, 1.0f32, 1.0f32,    1.0f32, 0.0f32, 0.0f32,
+        -1.0f32, 1.0f32,-1.0f32,   0.0f32, 1.0f32, 0.0f32,
+        -1.0f32, 1.0f32, 1.0f32,   0.0f32, 0.0f32, 1.0f32,
+        1.0f32, 1.0f32, 1.0f32,    1.0f32, 0.0f32, 0.0f32,
+        -1.0f32, 1.0f32, 1.0f32,   0.0f32, 1.0f32, 0.0f32,
+        1.0f32,-1.0f32, 1.0f32,    0.0f32, 0.0f32, 1.0f32,
     ];
     let quad_vertices_u8: &[u8] = core::slice::from_raw_parts(
         quad_vertices.as_ptr() as *const u8,
@@ -240,18 +297,12 @@ unsafe fn init_vertex_buffer(gl: &Context) -> (NativeBuffer, NativeBuffer, Nativ
     gl.bind_buffer(ARRAY_BUFFER, Some(quad_vbo));
     gl.buffer_data_u8_slice(ARRAY_BUFFER, quad_vertices_u8, STATIC_DRAW);
     gl.enable_vertex_attrib_array(0); //vec2 stone vertices positions
-    gl.vertex_attrib_pointer_f32(0, 2, FLOAT, false, 5*4, 0);
+    gl.vertex_attrib_pointer_f32(0, 3, FLOAT, false, 6*4, 0);
     gl.enable_vertex_attrib_array(1); //vec3 stone vertices color
-    gl.vertex_attrib_pointer_f32(1, 3, FLOAT, false, 5*4, 2*4);
+    gl.vertex_attrib_pointer_f32(1, 3, FLOAT, false, 6*4, 3*4);
 
     let instance_vbo = gl.create_buffer().unwrap();
 
     gl.bind_vertex_array(None);
     (quad_vbo, instance_vbo, vao)
-}
-
-unsafe fn set_uniform(gl: &Context, program: NativeProgram, name: &str, value: f32) {
-    let uniform_location = gl.get_uniform_location(program, name);
-    // See also `uniform_n_i32`, `uniform_n_u32`, `uniform_matrix_4_f32_slice` etc.
-    gl.uniform_1_f32(uniform_location.as_ref(), value)
 }
