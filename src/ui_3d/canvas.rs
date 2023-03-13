ac89a425fe0978aa55b331240de5d56c6415d91a
bgfxc4 <bindermarc@gmx.de>
2023-03-13 13:10:35 +0100

    added ground plane

--- src/ui_3d/canvas.rs
+++ src/ui_3d/canvas.rs
@@ -5,8 +5,12 @@ use crate::ui_3d::shaders;
 
 trait Renderable {
     unsafe fn destroy(&self, gl: &Context);
-    unsafe fn paint(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>);
-    unsafe fn fill_vertex_buffer(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>) -> usize;
+    unsafe fn paint(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>, light_pos: cgmath::Point3<f32>);
+    unsafe fn fill_vbo(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>, light_pos: cgmath::Point3<f32>) -> usize;
+    fn is_instanced(&self) -> bool;
+    unsafe fn fill_i_vbo(&self, _gl: &Context, _values: &Vec<f32>) {
+        panic!("This struct is not an instanced RenderObject and thus has no instanced vertex object");
+    }
 }
 
 struct RenderObject {
@@ -23,51 +27,27 @@ struct InstancedRenderObject {
 }
 
 impl Renderable for InstancedRenderObject {
-    unsafe fn destroy(&self, gl: &Context) { 
+    unsafe fn destroy(&self, gl: &Context) {
         gl.delete_program(self.program);
         gl.delete_vertex_array(self.vao);
         gl.delete_buffer(self.vbo);
         gl.delete_buffer(self.i_vbo);
     }
 
-    unsafe fn paint(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>) {
-        let count = self.fill_vertex_buffer(gl, mvp, cam_pos);
+    unsafe fn paint(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>, light_pos: cgmath::Point3<f32>) {
+        let count = self.fill_vbo(gl, mvp, cam_pos, light_pos);
 
         gl.draw_arrays_instanced(TRIANGLES, 0, 12*3, count.try_into().unwrap());
         // gl.draw_arrays(TRIANGLES, 0, 12*3);
         gl.bind_vertex_array(None);
     }
 
-    unsafe fn fill_vertex_buffer(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>) -> usize {
+    unsafe fn fill_vbo(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>, light_pos: cgmath::Point3<f32>) -> usize {
         gl.use_program(Some(self.program));
         gl.bind_vertex_array(Some(self.vao));
 
-        let mut translations: [f32; 200] = [0.0; 200];
-        let mut index = 0;
-        for y in -5..5 {
-            for x in -5..5 {
-                translations[index] = x as f32 * 3.50;
-                index += 1;
-                translations[index] = y as f32 * 3.50;
-                index += 1;
-            }
-        }
-        let translations_u8: &[u8] = core::slice::from_raw_parts(
-            translations.as_ptr() as *const u8,
-            translations.len() * core::mem::size_of::<f32>(),
-        );
-
-        gl.bind_buffer(ARRAY_BUFFER, Some(self.i_vbo));
-        gl.buffer_data_u8_slice(ARRAY_BUFFER, translations_u8, STATIC_DRAW);
-        gl.enable_vertex_attrib_array(2); //vec2 stone vertices positions
-        gl.vertex_attrib_pointer_f32(2, 2, FLOAT, false, 2*4, 0);
-
-        gl.bind_buffer(ARRAY_BUFFER, None);
-        gl.vertex_attrib_divisor(2, 1); // tell OpenGL this is an instanced vertex attribute
-
-
         let light_pos_location = gl.get_uniform_location(self.program, "lightPos");
-        let light_pos: [f32; 3] = [0.0, 2.0, 0.0];
+        let light_pos: [f32; 3] = [light_pos.x, light_pos.y, light_pos.z];
         gl.uniform_3_f32_slice(light_pos_location.as_ref(), &light_pos);
 
         let uniform_location = gl.get_uniform_location(self.program, "view_mat");
@@ -80,6 +60,27 @@ impl Renderable for InstancedRenderObject {
 
         100
     }
+
+    unsafe fn fill_i_vbo(&self, gl: &Context, values: &Vec<f32>) { 
+        gl.use_program(Some(self.program));
+        gl.bind_vertex_array(Some(self.vao));
+
+        let values_u8: &[u8] = core::slice::from_raw_parts(
+            values.as_slice().as_ptr() as *const u8,
+            values.len() * core::mem::size_of::<f32>(),
+        );
+
+        gl.bind_buffer(ARRAY_BUFFER, Some(self.i_vbo));
+        gl.buffer_data_u8_slice(ARRAY_BUFFER, values_u8, STATIC_DRAW);
+        gl.enable_vertex_attrib_array(2); //vec2 stone vertices positions
+        gl.vertex_attrib_pointer_f32(2, 2, FLOAT, false, 2*4, 0);
+
+        gl.bind_buffer(ARRAY_BUFFER, None);
+        gl.vertex_attrib_divisor(2, 1); // tell OpenGL this is an instanced vertex attribute
+
+    }
+
+    fn is_instanced(&self) -> bool { true }
 }
 
 impl Renderable for RenderObject {
@@ -89,33 +90,40 @@ impl Renderable for RenderObject {
         gl.delete_buffer(self.vbo);
     }
 
-    unsafe fn paint(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>) {    
-        self.fill_vertex_buffer(gl, mvp, cam_pos);
+    unsafe fn paint(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>, light_pos: cgmath::Point3<f32>) {    
+        self.fill_vbo(gl, mvp, cam_pos, light_pos);
 
         gl.draw_arrays(TRIANGLES, 0, 12*3);
         // gl.draw_arrays(TRIANGLES, 0, 12*3);
         gl.bind_vertex_array(None);
     }
 
-    unsafe fn fill_vertex_buffer(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, _cam_pos: cgmath::Point3<f32>) -> usize {
+    unsafe fn fill_vbo(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>, light_pos: cgmath::Point3<f32>) -> usize {
         gl.use_program(Some(self.program));
         gl.bind_vertex_array(Some(self.vao));
 
         let light_pos_location = gl.get_uniform_location(self.program, "light_pos");
-        let light_pos: [f32; 3] = [0.0, 2.0, 0.0];
+        let light_pos: [f32; 3] = [light_pos.x, light_pos.y, light_pos.z];
         gl.uniform_3_f32_slice(light_pos_location.as_ref(), &light_pos);
 
         let uniform_location = gl.get_uniform_location(self.program, "view_mat");
         let f32_mat: [[f32; 4]; 4] = mvp.into();
         gl.uniform_matrix_4_f32_slice(uniform_location.as_ref(), false, f32_mat.into_iter().flatten().collect::<Vec<f32>>().as_ref());
 
+        let cam_pos_location = gl.get_uniform_location(self.program, "camPos");
+        let cam_pos: [f32; 3] = [cam_pos.x, cam_pos.y, cam_pos.z];
+        gl.uniform_3_f32_slice(cam_pos_location.as_ref(), &cam_pos);
+
         1
     }
+
+    fn is_instanced(&self) -> bool { false }
 }
 
 pub struct Canvas {
     domino_obj: InstancedRenderObject,
-    light_obj: RenderObject
+    light_obj: RenderObject,
+    ground_obj: RenderObject,
 }
 
 #[allow(unsafe_code)] // we need unsafe code to use glow
@@ -123,11 +131,12 @@ impl Canvas {
     pub fn new(gl: &Context) -> Option<Self> {
         unsafe {
             // Create a vertex buffer and vertex array object
-            let (domino_obj, light_obj) = init_vertex_buffer(&gl);
+            let (domino_obj, light_obj, ground_obj) = init_vertex_buffer(&gl);
 
             Some(Self {
                 domino_obj,
-                light_obj
+                light_obj,
+                ground_obj
             })
         }
     }
@@ -136,10 +145,13 @@ impl Canvas {
         unsafe {
             self.domino_obj.destroy(gl);
             self.light_obj.destroy(gl);
+            self.ground_obj.destroy(gl);
         }
     }
 
     pub fn paint(&self, gl: &Context, mvp: cgmath::Matrix4<f32>, cam_pos: cgmath::Point3<f32>) {
+        let light_pos = cgmath::point3(0.0f32, 2.0f32, 1.0f32);
+
         unsafe {
             gl.enable(glow::DEPTH_TEST);
             // gl.enable(glow::CULL_FACE); TODO: might improve performance
@@ -148,8 +160,19 @@ impl Canvas {
 
             gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
         
-            self.domino_obj.paint(gl, mvp, cam_pos);
-            self.light_obj.paint(gl, mvp, cam_pos);
+            
+
+            let mut translations: Vec<f32> = vec![];
+            for y in -5..5 {
+                for x in -5..5 {
+                    translations.push(x as f32 * 3.50);
+                    translations.push(y as f32 * 3.50);
+                }
+            }
+            self.domino_obj.fill_i_vbo(gl, &translations);
+            self.domino_obj.paint(gl, mvp, cam_pos, light_pos);
+            self.light_obj.paint(gl, mvp, cam_pos, light_pos);
+            self.ground_obj.paint(gl, mvp, cam_pos, light_pos);
         }
     }
 }
@@ -194,7 +217,7 @@ unsafe fn create_program(
     program
 }
 
-unsafe fn init_vertex_buffer(gl: &Context) -> (InstancedRenderObject, RenderObject) {
+unsafe fn init_vertex_buffer(gl: &Context) -> (InstancedRenderObject, RenderObject, RenderObject) {
     // We now construct a vertex array to describe the format of the input buffer 
     let domino_program = create_program(&gl, shaders::dominos::VERTEX_SHADER, shaders::dominos::FRAGMENT_SHADER);
     let domino_vao = gl.create_vertex_array().unwrap();
@@ -243,5 +266,28 @@ unsafe fn init_vertex_buffer(gl: &Context) -> (InstancedRenderObject, RenderObje
     gl.bind_vertex_array(None);
     let light = RenderObject{vbo: light_vbo, vao: light_vao, program: light_program};
 
-    (dominos, light)
+
+    let ground_program = create_program(&gl, shaders::ground_plane::VERTEX_SHADER, shaders::ground_plane::FRAGMENT_SHADER);
+    let ground_vao = gl.create_vertex_array().unwrap();
+    gl.bind_vertex_array(Some(ground_vao));
+
+    let ground_vertices = shaders::ground_plane::VERTICES;
+    let ground_vertices_u8: &[u8] = core::slice::from_raw_parts(
+        ground_vertices.as_ptr() as *const u8,
+        ground_vertices.len() * core::mem::size_of::<f32>(),
+    );
+
+    // We construct a buffer
+    let ground_vbo = gl.create_buffer().unwrap();
+    gl.bind_buffer(ARRAY_BUFFER, Some(ground_vbo));
+    gl.buffer_data_u8_slice(ARRAY_BUFFER, ground_vertices_u8, STATIC_DRAW);
+    gl.enable_vertex_attrib_array(0); //vec2 stone vertices positions
+    gl.vertex_attrib_pointer_f32(0, 3, FLOAT, false, 6*4, 0);
+    gl.enable_vertex_attrib_array(1); //vec3 stone vertices normal
+    gl.vertex_attrib_pointer_f32(1, 3, FLOAT, false, 6*4, 3*4);
+
+    gl.bind_vertex_array(None);
+    let ground = RenderObject{vbo: ground_vbo, vao: ground_vao, program: ground_program};
+
+    (dominos, light, ground)
 }
--- src/ui_3d/shaders.rs
+++ src/ui_3d/shaders.rs
@@ -8,7 +8,6 @@ pub mod dominos {
 
         out vec3 Normal;
         out vec3 FragPos;
-        out vec3 vLightPos;
 
         void main()
         {
@@ -53,40 +52,40 @@ pub mod dominos {
         "#;
 
     pub const VERTICES: [f32; 216] = [
-        -0.5f32, -0.5f32, -0.5f32,  0.0f32,  0.0f32, -1.0f32,
-         0.5f32, -0.5f32, -0.5f32,  0.0f32,  0.0f32, -1.0f32, 
+        -0.5f32, 0.0f32, -0.5f32,  0.0f32,  0.0f32, -1.0f32,
+         0.5f32, 0.0f32, -0.5f32,  0.0f32,  0.0f32, -1.0f32, 
          0.5f32,  0.5f32, -0.5f32,  0.0f32,  0.0f32, -1.0f32, 
          0.5f32,  0.5f32, -0.5f32,  0.0f32,  0.0f32, -1.0f32, 
         -0.5f32,  0.5f32, -0.5f32,  0.0f32,  0.0f32, -1.0f32, 
-        -0.5f32, -0.5f32, -0.5f32,  0.0f32,  0.0f32, -1.0f32, 
+        -0.5f32, 0.0f32, -0.5f32,  0.0f32,  0.0f32, -1.0f32, 
 
-        -0.5f32, -0.5f32,  0.5f32,  0.0f32,  0.0f32, 1.0f32,
-         0.5f32, -0.5f32,  0.5f32,  0.0f32,  0.0f32, 1.0f32,
+        -0.5f32, 0.0f32,  0.5f32,  0.0f32,  0.0f32, 1.0f32,
+         0.5f32, 0.0f32,  0.5f32,  0.0f32,  0.0f32, 1.0f32,
          0.5f32,  0.5f32,  0.5f32,  0.0f32,  0.0f32, 1.0f32,
          0.5f32,  0.5f32,  0.5f32,  0.0f32,  0.0f32, 1.0f32,
         -0.5f32,  0.5f32,  0.5f32,  0.0f32,  0.0f32, 1.0f32,
-        -0.5f32, -0.5f32,  0.5f32,  0.0f32,  0.0f32, 1.0f32,
+        -0.5f32, 0.0f32,  0.5f32,  0.0f32,  0.0f32, 1.0f32,
 
         -0.5f32,  0.5f32,  0.5f32, -1.0f32,  0.0f32,  0.0f32,
         -0.5f32,  0.5f32, -0.5f32, -1.0f32,  0.0f32,  0.0f32,
-        -0.5f32, -0.5f32, -0.5f32, -1.0f32,  0.0f32,  0.0f32,
-        -0.5f32, -0.5f32, -0.5f32, -1.0f32,  0.0f32,  0.0f32,
-        -0.5f32, -0.5f32,  0.5f32, -1.0f32,  0.0f32,  0.0f32,
+        -0.5f32, 0.0f32, -0.5f32, -1.0f32,  0.0f32,  0.0f32,
+        -0.5f32, 0.0f32, -0.5f32, -1.0f32,  0.0f32,  0.0f32,
+        -0.5f32, 0.0f32,  0.5f32, -1.0f32,  0.0f32,  0.0f32,
         -0.5f32,  0.5f32,  0.5f32, -1.0f32,  0.0f32,  0.0f32,
 
          0.5f32,  0.5f32,  0.5f32,  1.0f32,  0.0f32,  0.0f32,
          0.5f32,  0.5f32, -0.5f32,  1.0f32,  0.0f32,  0.0f32,
-         0.5f32, -0.5f32, -0.5f32,  1.0f32,  0.0f32,  0.0f32,
-         0.5f32, -0.5f32, -0.5f32,  1.0f32,  0.0f32,  0.0f32,
-         0.5f32, -0.5f32,  0.5f32,  1.0f32,  0.0f32,  0.0f32,
+         0.5f32, 0.0f32, -0.5f32,  1.0f32,  0.0f32,  0.0f32,
+         0.5f32, 0.0f32, -0.5f32,  1.0f32,  0.0f32,  0.0f32,
+         0.5f32, 0.0f32,  0.5f32,  1.0f32,  0.0f32,  0.0f32,
          0.5f32,  0.5f32,  0.5f32,  1.0f32,  0.0f32,  0.0f32,
 
-        -0.5f32, -0.5f32, -0.5f32,  0.0f32, -1.0f32,  0.0f32,
-         0.5f32, -0.5f32, -0.5f32,  0.0f32, -1.0f32,  0.0f32,
-         0.5f32, -0.5f32,  0.5f32,  0.0f32, -1.0f32,  0.0f32,
-         0.5f32, -0.5f32,  0.5f32,  0.0f32, -1.0f32,  0.0f32,
-        -0.5f32, -0.5f32,  0.5f32,  0.0f32, -1.0f32,  0.0f32,
-        -0.5f32, -0.5f32, -0.5f32,  0.0f32, -1.0f32,  0.0f32,
+        -0.5f32, 0.0f32, -0.5f32,  0.0f32, -1.0f32,  0.0f32,
+         0.5f32, 0.0f32, -0.5f32,  0.0f32, -1.0f32,  0.0f32,
+         0.5f32, 0.0f32,  0.5f32,  0.0f32, -1.0f32,  0.0f32,
+         0.5f32, 0.0f32,  0.5f32,  0.0f32, -1.0f32,  0.0f32,
+        -0.5f32, 0.0f32,  0.5f32,  0.0f32, -1.0f32,  0.0f32,
+        -0.5f32, 0.0f32, -0.5f32,  0.0f32, -1.0f32,  0.0f32,
 
         -0.5f32,  0.5f32, -0.5f32,  0.0f32,  1.0f32,  0.0f32,
          0.5f32,  0.5f32, -0.5f32,  0.0f32,  1.0f32,  0.0f32,
@@ -162,5 +161,67 @@ pub mod light_source {
          0.1f32,  0.1f32,  0.1f32,  0.0f32,  1.0f32,  0.0f32,
         -0.1f32,  0.1f32,  0.1f32,  0.0f32,  1.0f32,  0.0f32,
         -0.1f32,  0.1f32, -0.1f32,  0.0f32,  1.0f32,  0.0f32
+    ];
+}
+
+pub mod ground_plane {
+    pub const VERTEX_SHADER: &str = r#"#version 330 core
+        layout (location = 0) in vec3 pos_model_space;
+        layout (location = 1) in vec3 aNormal;
+
+        uniform mat4 view_mat;
+
+        out vec3 Normal;
+        out vec3 FragPos;
+
+        void main()
+        {
+            vec3 pos_world_space = pos_model_space;
+            Normal = aNormal;
+            FragPos = pos_world_space;
+            gl_Position = view_mat * vec4(pos_world_space, 1.0);
+        }
+        "#;
+    pub const FRAGMENT_SHADER: &str =
+        r#"#version 330 core
+        out vec4 FragColor;
+
+        in vec3 Normal;
+        in vec3 FragPos;
+
+        uniform vec3 lightPos;
+        uniform vec3 camPos;
+
+        void main()
+        {
+            vec3 lightColor = vec3(1.0, 1.0, 1.0);
+            vec3 objectColor = vec3(0.0, 1.0, 1.0);
+            
+            float ambientStrength = 0.1;
+            vec3 ambient = ambientStrength * lightColor;
+
+            vec3 norm = normalize(Normal);
+            vec3 lightDir = normalize(lightPos - FragPos);
+            float diff = max(dot(norm, lightDir), 0.0);
+            vec3 diffuse = diff * lightColor;
+
+            float specularStrength = 0.5;
+            vec3 viewDir = normalize(camPos - FragPos);
+            vec3 reflectDir = reflect(-lightDir, norm);
+            float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+            vec3 specular = specularStrength * spec * lightColor;
+
+            vec3 result = (diffuse + ambient + specular) * objectColor;
+            FragColor = vec4(result, 1.0);
+        }
+        "#;
+
+    pub const VERTICES: [f32; 36] = [
+        -20f32,  0f32, -20f32,  0.0f32,  1.0f32,  0.0f32,
+         20f32,  0f32, -20f32,  0.0f32,  1.0f32,  0.0f32,
+         20f32,  0f32,  20f32,  0.0f32,  1.0f32,  0.0f32,
+         20f32,  0f32,  20f32,  0.0f32,  1.0f32,  0.0f32,
+        -20f32,  0f32,  20f32,  0.0f32,  1.0f32,  0.0f32,
+        -20f32,  0f32, -20f32,  0.0f32,  1.0f32,  0.0f32
     ];
 }
